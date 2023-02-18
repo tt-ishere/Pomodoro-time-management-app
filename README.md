d4176d8a3f76248699cefa8e9c9450ed170a1e54
bizzy-Code <63963271+bizzy-Code@users.noreply.github.com>
2023-02-18 12:36:02 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pomodoro-time-management-app
+ This a simple yet functional GUI for a Pomodoro timer, which allows users to manage their time more efficiently.
---
+++ main.py
@@ -0,0 +1,105 @@
+from tkinter import *
+import math
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+reps = 0
+timer = None
+marks = None
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    tittle_label.config(text="Timer")
+    global marks
+    marks = ""
+    checkmark_label.config(text=marks, fg=GREEN)
+    
+    
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    global reps
+    reps += 1
+    
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_min = LONG_BREAK_MIN * 60
+    
+    
+    if reps % 8 == 0:
+        tittle_label.config(text="Break", fg=RED)
+        count_down(long_break_min)
+    
+    elif reps %  2 == 0:
+        tittle_label.config(text="Break", fg=PINK)
+        count_down(short_break_sec)
+     
+    else:
+        tittle_label.config(text="Work", fg=GREEN)
+        count_down(work_sec)
+        
+    
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = math.floor(count / 60)
+    count_sec = round(count % 60)
+    
+    if count_sec < 10 :
+        count_sec = f"0{count_sec}"
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    
+    if count > 0:
+        global timer
+        timer = window.after(1000, count_down, count -1)
+    else:
+        work_sessions = math.floor(reps/2)
+        global marks
+        marks = ""
+        for _ in range(work_sessions):
+            marks += "✔"
+        checkmark_label.config(text=marks)
+        window.lift()
+        window.focus_force()
+        start_timer()
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 114, image=tomato_img)
+timer_text = canvas.create_text(100, 135, text="00:00", fill="white", font=(FONT_NAME, 35,"bold"))
+canvas.grid(column=1, row=1)
+
+tittle_label = Label(text="Timer", font=(FONT_NAME, 40,"bold"), fg=GREEN, bg=YELLOW)
+tittle_label.grid(column=1, row=0)
+
+start_button = Button(text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_timer)
+reset_button.grid(column=2, row=2)
+
+checkmark_label = Label(fg=GREEN, bg=YELLOW)
+checkmark_label.grid(column=1, row=3)
+
+
+
+window.mainloop()
